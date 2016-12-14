7f859de7b5a8235966708cfccbca9215c1ca7b18
pi <pv97@cornell.edu>
2016-12-13 21:54:08 -0800

    fix featured list

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -74379,7 +74379,7 @@
 	
 	    _this.state = {
 	      featureTimer: 0,
-	      featured: [{ class: "picture1", videoId: "52", name: "How Far Will We Go?" }, { class: "picture2", videoId: "53", name: "Atoms As Big As Mountains" }, { class: "picture3", videoId: "54", name: "What is Light?" }, { class: "picture4", videoId: "55", name: "How the Stock Exchange Works" }, { class: "picture5", videoId: "3", name: "How Big is the Moon?" }]
+	      featured: [{ class: "picture1", videoId: "53", name: "How Far Will We Go?" }, { class: "picture2", videoId: "54", name: "Atoms As Big As Mountains" }, { class: "picture3", videoId: "55", name: "What is Light?" }, { class: "picture4", videoId: "56", name: "How the Stock Exchange Works" }, { class: "picture5", videoId: "3", name: "How Big is the Moon?" }]
 	    };
 	    _this.onSplashClick = _this.onSplashClick.bind(_this);
 	    _this.onChannelClick = _this.onChannelClick.bind(_this);
--- frontend/components/video_index/video_index.jsx
+++ frontend/components/video_index/video_index.jsx
@@ -8,10 +8,10 @@ class VideoIndex extends React.Component {
     this.state = {
       featureTimer:0,
       featured:[
-        {class:"picture1",videoId:"52",name:"How Far Will We Go?"},
-        {class:"picture2",videoId:"53",name:"Atoms As Big As Mountains"},
-        {class:"picture3",videoId:"54",name:"What is Light?"},
-        {class:"picture4",videoId:"55",name:"How the Stock Exchange Works"},
+        {class:"picture1",videoId:"53",name:"How Far Will We Go?"},
+        {class:"picture2",videoId:"54",name:"Atoms As Big As Mountains"},
+        {class:"picture3",videoId:"55",name:"What is Light?"},
+        {class:"picture4",videoId:"56",name:"How the Stock Exchange Works"},
         {class:"picture5",videoId:"3",name:"How Big is the Moon?"}
       ]
     }
